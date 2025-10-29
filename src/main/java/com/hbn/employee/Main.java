b924726033914b087a27ed6609bbac9eb4dbd939
Souvik7602 <sm2496444@gmail.com>
2025-10-29 14:59:03 +0530

    update

--- src/main/java/com/hbn/employee/Main.java
+++ src/main/java/com/hbn/employee/Main.java
@@ -2,14 +2,13 @@ package com.hbn.employee;
 
 
 import org.hibernate.Session;
-//import org.hibernate.SessionFactory;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 //import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
+//import org.hibernate.boot.MetadataSources;
 //import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
-
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import com.hbn.employee.configuration.Hibernateconfiguration;
 import com.hbn.employee.entity.Employee;
 
 public class Main {
@@ -20,7 +19,9 @@ public class Main {
 //		Metadata meta=new MetadataSources(ssr).getMetadataBuilder().build();
 //		SessionFactory sessionFactory=meta.buildSessionFactory();
 //		Session session = sessionFactory.openSession();
-		Session session =new MetadataSources(new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build()).getMetadataBuilder().build().buildSessionFactory().openSession();
+		
+//		SessionFactory sessionFactory= Hibernateconfiguration.getSessionFactory();
+		Session session =Hibernateconfiguration.getSessionFactory().openSession();
 		Transaction tx = session.beginTransaction();
 		
 		session.persist(emp);
---
+++ src/main/java/com/hbn/employee/configuration/Hibernateconfiguration.java
@@ -0,0 +1,29 @@
+package com.hbn.employee.configuration;
+
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+//import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+//import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+public class Hibernateconfiguration {
+	public static SessionFactory getSessionFactory() {
+		Properties prop=new Properties();
+		prop.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+		prop.put(Environment.JAKARTA_JDBC_URL,"jdbc:mysql://localhost:3306/bhanu");
+		prop.put(Environment.JAKARTA_JDBC_USER, "root");
+		prop.put(Environment.JAKARTA_JDBC_PASSWORD, "souvik@7602");
+		prop.put(Environment.HBM2DDL_AUTO, "create");
+		prop.put(Environment.SHOW_SQL, "true");
+		prop.put(Environment.FORMAT_SQL, "true");
+		
+//		StandardServiceRegistry ssr=new StandardServiceRegistryBuilder().applySettings(prop).build();
+//		Metadata meta=new MetadataSources(ssr).addAnnotatedClass(com.hbn.employee.entity.Employee.class).buildMetadata();
+//		SessionFactory sessionFactory=meta.buildSessionFactory();
+
+		return new MetadataSources(new StandardServiceRegistryBuilder().applySettings(prop).build()).addAnnotatedClass(com.hbn.employee.entity.Employee.class).buildMetadata().buildSessionFactory();
+	}
+
+}
